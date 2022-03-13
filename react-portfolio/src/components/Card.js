0c4b56868e20b9c10f4f77f7aa85a95964549c82
Mona Lisa <mekotigi@gmail.com>
2022-03-13 16:29:30 -0400

    add all cards

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -4,19 +4,94 @@ export default function Cards() {
 	return (
 		<div className="container">
 			<div className="row justify-content-center">
-			<div className="card bg-dark border-primary" style={{ width: '18rem' }}>
+
+				<div className="card bg-dark border-primary" style={{ width: '18rem' }}>
 					<img src="./images/movieGrab.png" className="card-img-top" alt="..." />
 					<div className="card-body">
-						<h5 className="cardTitle text-center">Movie Grab</h5>
+						<h5 className="cardTitle text-center">lyrics-finder</h5>
 						{/* <p className="cardText">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
 						<div className="container">
 							<div className="row justify-content-center">
 								<a href="https://chamibia.github.io/lyrics-finder/" className="cardBtn">Website</a>
-								<a href="https://mekonnenke.github.io/DiceChallengeofA/" className="cardBtn">Source</a>
+								<a href="https://github.com/chamibia/lyrics-finder/pull/9" className="cardBtn">Source</a>
 							</div>
 						</div>
 					</div>
 				</div>
+
+				<div className="card bg-dark border-primary" style={{ width: '18rem' }}>
+					<img src="./images/brew3.png" className="card-img-top brewImg" alt="..." />
+					<div className="card-body">
+						<h5 className="cardTitle text-center">MVC Tech Bloger</h5>
+						{/* <p className="cardText">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+						<div className="container">
+							<div className="row justify-content-center">
+								<a href="https://pure-cliffs-54858.herokuapp.com/" className="cardBtn">Website</a>
+								<a href="https://github.com/Mekonnenke/MVC-TECH_BLOG" className="cardBtn">Source</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="card bg-dark border-primary" style={{ width: '18rem' }}>
+					<img src="./images/budget.png" className="card-img-top budgetImg" alt="..." />
+					<div className="card-body">
+						<h5 className="cardTitle text-center"> Express Note Taker</h5>
+						{/* <p className="cardText">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+						<div className="container">
+							<div className="row justify-content-center">
+								<a href=" https://desolate-headland-54764.herokuapp.com/notes" className="cardBtn">Website</a>
+								<a href="https://github.com/Mekonnenke/express_note_taker" className="cardBtn">Source</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div className="row justify-content-center">
+
+				<div className="card bg-dark border-primary" style={{ width: '18rem' }}>
+					<img src="./images/workout.png" className="card-img-top workoutImg" alt="..." />
+					<div className="card-body">
+						<h5 className="cardTitle text-center">TingDong</h5>
+						{/* <p className="cardText">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+						<div className="container">
+							<div className="row justify-content-center">
+								<a href=" https://mekonnenke.github.io/TingDong-Project/" className="cardBtn">Website</a>
+								<a href="https://github.com/Mekonnenke/TingDong-Project" className="cardBtn">Source</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="card bg-dark border-primary" style={{ width: '18rem' }}>
+					<img src="./images/tech.png" className="card-img-top techImg" alt="..." />
+					<div className="card-body">
+						<h5 className="cardTitle text-center">Weather Dashboard</h5>
+						{/* <p className="cardText">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+						<div className="container">
+							<div className="row justify-content-center">
+								<a href="https://mekonnenke.github.io/WeatherDashBoard/" className="cardBtn">Website</a>
+								<a href="https://github.com/Mekonnenke/WeatherDashBoard" className="cardBtn">Source</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="card bg-dark border-primary" style={{ width: '18rem' }}>
+					<img src="./images/note.png" className="card-img-top noteImg" alt="..." />
+					<div className="card-body">
+						<h5 className="cardTitle text-center">Taskination</h5>
+						{/* <p className="cardText">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+						<div className="container">
+							<div className="row justify-content-center">
+								<a href="https://mekonnenke.github.io/HTML_CSS-_challengeAssignmnet/" className="cardBtn">Website</a>
+								<a href="https://github.com/Mekonnenke/HTML_CSS-_challengeAssignmnet" className="cardBtn">Source</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	);
